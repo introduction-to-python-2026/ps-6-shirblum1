f343271aec1de2c6822e3890232d977cbecace0a
shirblum1 <shirblum1@gmail.com>
2025-11-27 13:04:02 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,9 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    codon2amino_acid_dict = {}
+    with open(file_path) as codons_file:
+        codon2amino_acid_dict = {
+            row.strip().split('\t')[0]: row.strip().split('\t')[2]
+            for row in codons_file.readlines()}
+    return codon2amino_acid_dict
 
 
